9cc5039255e9f1f435121ab031f6b6d6a2c31c7a
David Han <handyj443@gmail.com>
2017-08-19 18:51:34 +0100

    Edge detection

--- MyLittleProgram/MyLittleProgram/shaders/fullScreenQuad.fs
+++ MyLittleProgram/MyLittleProgram/shaders/fullScreenQuad.fs
@@ -22,11 +22,11 @@ void main()
         vec2( offset, -offset)  // bottom-right    
     );
 
-	float kernel[9] = float[](
-	    1.0 / 16, 2.0 / 16, 1.0 / 16,
-	    2.0 / 16, 4.0 / 16, 2.0 / 16,
-	    1.0 / 16, 2.0 / 16, 1.0 / 16  
-	);
+    float kernel[9] = float[](
+        1, 1,  1,
+        1, -8, 1,
+        1, 1,  1
+    );
     
     vec3 sampleTex[9];
     for(int i = 0; i < 9; i++)
